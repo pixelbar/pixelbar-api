f71f7e8fae45c83adf8ba27fa891606ae424e6a4
David van Leeuwen <david@madeawkward.com>
2016-01-10 22:18:10 +0100

    Use correct port

--- src/app.js
+++ src/app.js
@@ -32,5 +32,6 @@ app.use(function *(next) {
 app.use(mount('/v1', api.v1.call(app)))
 app = http.createServer(app.callback())
 
-app.listen(3000)
-console.log('$ open http://127.0.0.1:3000')
+var port = process.env.NODE_ENV ? 3000 : 80
+app.listen(port)
+console.log(`$ open http://127.0.0.1:${port}`)
